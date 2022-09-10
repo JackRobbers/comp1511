034f5f480f1305743901a49e868bc95d116bf857
Jack Robbers <jack@2pisoftware.com>
2022-09-10 21:16:47 +1000

    complete expressions

--- 02/expressions.c
+++ 02/expressions.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("value of `(7/2)` is 
-    printf("value of `(3.0 / 2) + 1`
-    printf("value of `'a' + 5`
-    printf("value of `'F' - 'A' + 'a'`
+    printf("value of (7/2) is %d\n", (7/2));
+    printf("value of (3.0 / 2) + 1 is %lf\n", (3.0 / 2) + 1);
+    printf("value of 'a' + 5 is %c", 'a' + 5);
+    printf("value of 'F' - 'A' + 'a' is %c", 'F' - 'A' + 'a');
     return  0;
 }
