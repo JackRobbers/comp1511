78cf2c36aeb514a2227fe3f89d86b2f86ec4f1ac
Jack Robbers <jack@robbers.net.au>
2022-09-10 21:13:25 +1000

    add expressions code

---
+++ 02/expressions.c
@@ -0,0 +1,11 @@
+// print out expressions from tute 02
+
+#include <stdio.h>
+
+int main(void) {
+    printf("value of `(7/2)` is 
+    printf("value of `(3.0 / 2) + 1`
+    printf("value of `'a' + 5`
+    printf("value of `'F' - 'A' + 'a'`
+    return  0;
+}
