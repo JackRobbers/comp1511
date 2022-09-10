fdbcc9b703ef0b19f81642990b5966d83fd4ae27
Jack Robbers <jack@2pisoftware.com>
2022-09-10 21:18:46 +1000

    newlines in expressions

--- 02/expressions.c
+++ 02/expressions.c
@@ -5,7 +5,7 @@
 int main(void) {
     printf("value of (7/2) is %d\n", (7/2));
     printf("value of (3.0 / 2) + 1 is %lf\n", (3.0 / 2) + 1);
-    printf("value of 'a' + 5 is %c", 'a' + 5);
-    printf("value of 'F' - 'A' + 'a' is %c", 'F' - 'A' + 'a');
-    return  0;
+    printf("value of 'a' + 5 is %c\n", 'a' + 5);
+    printf("value of 'F' - 'A' + 'a' is %c\n", 'F' - 'A' + 'a');
+    return 0;
 }
