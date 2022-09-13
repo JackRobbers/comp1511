2a8b274e6f940dc540d567edcb312353d483d7c2
Jack Robbers <jack@robbers.net.au>
2022-09-13 10:26:51 +1000

    tute code

---
+++ 01/cat_face.c
@@ -0,0 +1,12 @@
+// draw a cat face
+// Jack Robbers z5263682 2022.09.13
+
+#include <stdio.h>
+
+int main(void) {
+                                    
+    printf(" /\\___/\\\n");
+    printf("(=' w '=)\n");
+
+    return 0;
+}
