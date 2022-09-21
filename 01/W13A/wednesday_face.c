0372ed807c5dbb2fd4a5784eb6588b40d7075ac9
Jack Robbers <jack@robbers.net.au>
2022-09-21 12:57:55 +1000

    cse

---
+++ 01/W13A/wednesday_face.c
@@ -0,0 +1,12 @@
+// draws a cat face
+// Jack Robbers z5263682, september 2022
+
+#include <stdio.h>
+
+int main(void) {
+
+    printf(" /\\___/\\\n");
+    printf("(=' w '=)\n");
+
+    return 0;
+}
