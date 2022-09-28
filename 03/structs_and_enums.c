b4245fa8e6fabdcef5420544caf961424e0ecc0c
Jack Robbers <jack@robbers.net.au>
2022-09-28 13:53:08 +1000

    add tute code w13a

--- 03/structs_and_enums.c
+++ 03/structs_and_enums.c
@@ -4,6 +4,7 @@ struct person {
     int shoe_size;
     double height;
     char first_name_initial;
+    enum opal_card_type card;
 };
 
 enum opal_card_type {
@@ -11,7 +12,40 @@ enum opal_card_type {
     STUDENT,
     CONCESSION
 };
+
+// create a struct person and return it.
+struct person new_person(int shoe_size, double height, char first_name_initial);
+void print_person(struct person to_print);
+
 int main(void) {
-    
+    struct person jack = new_person(32, 172.0, 'j');
+
+    struct person arpita = new_person(30, 200.0, 'a');
+
+    print_person(jack);
+
     return 0;
+}
+
+struct person new_person(int shoe_size, double height, char first_name_initial) {
+    // if (first_name_initial < 'a' || first_name_initial > 'z') {
+    //     printf("invalid initial\n");
+    //     // means there is no value, will cover this in more detail later
+    //     return NULL;
+    // }
+    struct person new;
+    new.shoe_size = shoe_size;
+    new.height = height;
+    new.first_name_initial = first_name_initial;
+    new.card = STUDENT;
+    return new;
+}
+
+void print_person(struct person to_print) {
+    printf(
+        "This persons height is %lf, shoe size is %d, and their first initial is %c\n", 
+        to_print.height, 
+        to_print.shoe_size, 
+        to_print.first_name_initial
+    );
 }
