48fba56f68004f5cfa158b815f14a194881d3d37
Jack Robbers <jack@2pisoftware.com>
2022-11-05 11:15:43 +1100

    update refactor comment

--- 09/refactor.c
+++ 09/refactor.c
@@ -28,8 +28,9 @@ void place_log(struct board_tile board[SIZE][SIZE]) {
     // guaranteed that start_column <= end_column so don't 
     // have to check start_column < SIZE && end_column > 0
 
+    // this code would pass 1511 style, but isn't good style
     // how would you fix the style here?
-    // (there's also a subtle bug, can you spot it?)
+    // (there's also a bug, can you spot it?)
     if (!turtles_in_row 
         && row > 0 
         && row < SIZE - 1 
