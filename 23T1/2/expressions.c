a48c0bd5ad226dc13e851952242c4ec677d701a0
Jack Robbers <jack@2pisoftware.com>
2023-02-20 11:18:23 +1100

    add week 2 tute

---
+++ 23T1/2/expressions.c
@@ -0,0 +1,11 @@
+// print out expressions from tute 02
+
+#include <stdio.h>
+
+int main(void) {
+    printf("value of (7/2) is %d\n", (7/2));
+    printf("value of (3.0 / 2) + 1 is %lf\n", (3.0 / 2) + 1);
+    printf("value of 'a' + 5 is %c\n", 'a' + 5);
+    printf("value of 'F' - 'A' + 'a' is %c\n", 'F' - 'A' + 'a');
+    return 0;
+}
