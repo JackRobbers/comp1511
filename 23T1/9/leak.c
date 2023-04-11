b2cece05325155622a141b428d9fdc45c1efbd67
Jack Robbers <jack@robbers.net.au>
2023-04-11 14:57:24 +1000

    create week 9 content

---
+++ 23T1/9/leak.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(void) {
+    printf("Type y to start allocating heaps of memory ");
+    scanf(" ");
+    printf("doing it\n");
+    while (1) {
+        
+        malloc(10000);
+    }
+}
